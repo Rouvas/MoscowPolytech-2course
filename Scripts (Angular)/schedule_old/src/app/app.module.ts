5b4b868fcd6f0763e37be7e06f7bb7737ad4cc58
Rouvas799 <mail@gaiduchik.com>
2020-03-01 00:40:33 +0300

    Some problems

--- Scripts (Angular)/schedule_old/src/app/app.module.ts
+++ Scripts (Angular)/schedule_old/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
+=======
+import { ClassscheduleComponent } from './classschedule/classschedule.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+>>>>>>> parent of 38793e55... nn
 
 @NgModule({
   declarations: [
@@ -9,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+<<<<<<< HEAD
     NgxSmartModalModule.forRoot()
+=======
+    NgxSmartModalModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase)
+>>>>>>> parent of 38793e55... nn
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Scripts (Angular)/schedule_old/src/environments/environment.ts
+++ Scripts (Angular)/schedule_old/src/environments/environment.ts
@@ -2,9 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+<<<<<<< HEAD
 export const environment = {
   production: false
 };
+=======
+
+
+>>>>>>> parent of 38793e55... nn
 
 /*
  * For easier debugging in development mode, you can import the following file
