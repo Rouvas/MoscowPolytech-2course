ce763502883187a2ca185278a534890d40fef0f4
Rouvas <mail@gaiduchik.com>
2020-05-16 11:39:26 +0300

    some changes

---
+++ Програмная инженерия/Лабораторная работа №6/README.md
@@ -0,0 +1,7 @@
+Лабораторная работа №6 <br>
+Видео + код  <br>
+
+
+
+
+iframe width="560" height="315" src="https://www.youtube.com/embed/vqLEFxz9sks" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
---
+++ Програмная инженерия/Лабораторная работа №6/ocart2/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/ocart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/ocart2/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Program Files/Ampps/www/ocart2/catalog/');
+define('DIR_SYSTEM', 'C:/Program Files/Ampps/www/ocart2/system/');
+define('DIR_IMAGE', 'C:/Program Files/Ampps/www/ocart2/image/');
+define('DIR_STORAGE', 'C:/Program Files/Ampps/private/ocartdata/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ocar388');
+define('DB_PASSWORD', '1pa89([S7T');
+define('DB_DATABASE', 'ocar388');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc80_');
